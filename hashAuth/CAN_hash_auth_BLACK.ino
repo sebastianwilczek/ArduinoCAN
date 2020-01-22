4ad984bab2f10eef676d4d84e0b6b044c91586ad
Sebastian Wilczek <sebastian.wilczek@protonmail.com>
2020-01-22 10:26:27 +0100

    Added Hash Auth Scripts

---
+++ hashAuth/CAN_hash_auth_BLACK.ino
@@ -0,0 +1,139 @@
+// demo: CAN-BUS Shield, receive data with check mode
+// send data coming to fast, such as less than 10ms, you can use this way
+// loovee, 2014-6-13
+
+#include <SPI.h>
+#include "mcp_can.h"
+
+/*SAMD core*/
+#ifdef ARDUINO_SAMD_VARIANT_COMPLIANCE
+  #define SERIAL SerialUSB
+#else
+  #define SERIAL Serial
+#endif
+
+// the cs pin of the version after v1.1 is default to D9
+// v0.9b and v1.0 is default D10
+const int SPI_CS_PIN = 9;
+const int LED        = 8;
+boolean ledON        = 1;
+
+MCP_CAN CAN(SPI_CS_PIN);                                    // Set CS pin
+
+// Hash Based Authentication
+int groupId = 1;
+int counter = 0;
+int keyCounter = 0;
+unsigned long presharedKey = 84364362;
+unsigned long generatedKey;
+unsigned char values[8];
+
+void setup()
+{
+    SERIAL.begin(115200);
+    pinMode(LED,OUTPUT);
+
+    while (CAN_OK != CAN.begin(CAN_500KBPS))              // init can bus : baudrate = 500k
+    {
+        SERIAL.println("CAN BUS Shield init fail");
+        SERIAL.println("Init CAN BUS Shield again");
+        delay(100);
+    }
+    SERIAL.println("CAN BUS Shield init ok!");
+
+    SERIAL.println("Setting up Hash Authentication.");
+    generatedKey = generateKey(presharedKey, 0);
+    SERIAL.print("First generated key: ");
+    SERIAL.println(generatedKey);
+    
+    delay(1000);
+}
+
+
+void loop()
+{
+    unsigned char len = 0;
+    unsigned char buf[8];   
+
+    if(CAN_MSGAVAIL == CAN.checkReceive())            // check if data coming
+    {
+        CAN.readMsgBuf(&len, buf);    // read data,  len: data length, buf: data buf
+
+        unsigned long canId = CAN.getCanId();
+
+        SERIAL.println("-----------------------------");
+        SERIAL.print("get data from ID: 0x");
+        SERIAL.println(canId, HEX);
+        SERIAL.println(buf[7], HEX);
+
+        if(canId == 0x01){
+            SERIAL.print("Calculated hash: ");
+            SERIAL.println(hash(buf[7]), HEX);
+            SERIAL.print("Received hash: ");
+            SERIAL.println(buf[4] + (buf[5] << 8) + (buf[6] << 16), HEX);
+
+            unsigned long hashedValue = hash(buf[7]);
+            values[0] = hashedValue;
+            values[1] = hashedValue >> 8;
+            values[2] = hashedValue >> 16;
+
+            if(values[0] != buf[4] || values[1] != buf[5] || values[2] != buf[6]){
+                SERIAL.println("Hash Unmatch. Potential compromised ECU.");
+            } else {
+                counter++;
+                SERIAL.println("Received vaild brake message.");
+                if(buf[7] == 0x01){
+                    SERIAL.println("Brakes engaged.");
+                } else {
+                    SERIAL.println("Brakes loosened.");
+                }
+            }
+        }
+        if(canId == 0x02){
+            if (buf[7] == keyCounter+1)
+            {
+                unsigned long randomValue = buf[5] + (buf[6] << 8);
+
+                Serial.print("Received Random Value: ");
+                Serial.println(randomValue, HEX);
+
+                unsigned long keyRenewHashRes = keyRenewHash(generatedKey, randomValue, keyCounter+1)
+                //unsigned long keyRenewHashRes = buf[2] + (buf[3] << 8) + (buf[4] << 16);
+                values[2] = keyRenewHashRes;
+                values[3] = keyRenewHashRes >> 8;
+                values[4] = keyRenewHashRes >> 16;
+
+                Serial.print("Received Key Hash: ");
+                Serial.println(keyRenewHashRes, HEX);
+
+                if(values[2] == buf[2] && values[3] == buf[3] && values[4] == buf[4]){
+                    keyCounter++;
+                    generatedKey = generateKey(generatedKey, randomValue);
+                    SERIAL.print("New generated key is ");
+                    SERIAL.println(generatedKey, HEX);
+                } else {
+                    SERIAL.println("Key Renewal Hash mismatch, ignoring key renewal");
+                }
+            } else {
+                SERIAL.println("Counter out of sync, ignoring key renewal");
+            }
+        }
+    }
+}
+
+unsigned long hash(long data)
+{
+  return (groupId + generatedKey + counter + data) * 5325 % 16777216;
+}
+
+unsigned long generateKey(long previousKey, long randomValue)
+{
+  return (previousKey + randomValue + keyCounter) % 100000000;
+}
+
+unsigned long keyRenewHash(long genKey, long rValue, int c)
+{
+  return (genKey + rValue + c) * 5325 % 16777216;
+}
+
+//END FILE
---
+++ hashAuth/SEND_hash_auth.ino
@@ -0,0 +1,204 @@
+#include <Canbus.h>
+#include <defaults.h>
+#include <global.h>
+#include <mcp2515.h>
+#include <mcp2515_defs.h>
+#include <SHA3.h>
+
+/* Define Joystick connection pins */
+#define UP     A1
+#define DOWN   A3
+#define LEFT   A2
+#define RIGHT  A5
+#define CLICK  A4
+
+// Hash Based Authentication
+int groupId = 1;
+int counter = 0;
+int keyCounter = 0;
+unsigned long presharedKey = 84364362;
+unsigned long generatedKey;
+unsigned long randomValue;
+unsigned char values[8];
+
+void setup() {
+  //Initialize analog pins as inputs
+  pinMode(UP,INPUT);
+  pinMode(DOWN,INPUT);
+  pinMode(LEFT,INPUT);
+  pinMode(RIGHT,INPUT);
+  pinMode(CLICK,INPUT);
+  
+  //Pull analog pins high to enable reading of joystick movements
+  digitalWrite(UP, HIGH);
+  digitalWrite(DOWN, HIGH);
+  digitalWrite(LEFT, HIGH);
+  digitalWrite(RIGHT, HIGH);
+  digitalWrite(CLICK, HIGH);
+ 
+ //Initialize serial terminal connection
+  Serial.begin(9600);
+
+ if(Canbus.init(CANSPEED_500))  //Initialise MCP2515 CAN controller at the specified speed
+    Serial.println("CAN Init ok");
+  else
+    Serial.println("Can't init CAN");
+
+  Serial.println("Setting up Hash Authentication.");
+  generatedKey = generateKey(presharedKey, 0);
+  Serial.print("First generated key: ");
+  Serial.println(generatedKey, HEX);
+    
+  delay(1000);
+}
+
+void loop() {
+  tCAN message;
+  //Scan analog pins. If pin reads low, print the corresponding joystick movement.
+  if (digitalRead(UP) == 0) {
+    Serial.println("Up");
+
+    //Hash Auth
+    unsigned long hashedValue = hash(1);
+    values[0] = hashedValue;
+    values[1] = hashedValue >> 8;
+    values[2] = hashedValue >> 16;
+    values[3] = hashedValue >> 24;
+
+    Serial.print("Computed Hash: ");
+    Serial.println(hashedValue, HEX);
+
+    message.id = 0x01;
+    message.header.rtr = 0;
+    message.header.length = 8;
+    message.data[0] = 0x00;
+    message.data[1] = 0x00;
+    message.data[2] = 0x00;
+    message.data[3] = 0x00;
+    message.data[4] = values[0];
+    message.data[5] = values[1];
+    message.data[6] = values[2];
+    message.data[7] = 0x01;
+    mcp2515_bit_modify(CANCTRL, (1<<REQOP2)|(1<<REQOP1)|(1<<REQOP0), 0);
+    mcp2515_send_message(&message);
+    counter++;
+  }
+    
+  if (digitalRead(DOWN) == 0) {
+    Serial.println("Down");
+
+    //Hash Auth
+    unsigned long hashedValue = hash(1);
+    values[0] = hashedValue;
+    values[1] = hashedValue >> 8;
+    values[2] = hashedValue >> 16;
+    values[3] = hashedValue >> 24;
+
+    Serial.print("Computed Hash: ");
+    Serial.println(hashedValue, HEX);
+
+    message.id = 0x01;
+    message.header.rtr = 0;
+    message.header.length = 8;
+    message.data[0] = 0x00;
+    message.data[1] = 0x00;
+    message.data[2] = 0x00;
+    message.data[3] = 0x00;
+    message.data[4] = values[0];
+    message.data[5] = values[1];
+    message.data[6] = values[2];
+    message.data[7] = 0x00;
+    mcp2515_bit_modify(CANCTRL, (1<<REQOP2)|(1<<REQOP1)|(1<<REQOP0), 0);
+    mcp2515_send_message(&message);
+    counter++;
+  }
+    
+  if (digitalRead(LEFT) == 0) {
+    Serial.println("Left");
+
+    Serial.println("Starting key renewal");
+    renewKey();
+  }
+  
+  if (digitalRead(RIGHT) == 0) {
+    Serial.println("Right");
+
+    //Hacked Hash Auth
+    unsigned long hashedValue = rand() % 16777216;
+    values[0] = hashedValue;
+    values[1] = hashedValue >> 8;
+    values[2] = hashedValue >> 16;
+
+    Serial.print("Random Hash: ");
+    Serial.println(hashedValue, HEX);
+
+    message.id = 0x01;
+    message.header.rtr = 0;
+    message.header.length = 8;
+    message.data[0] = 0x00;
+    message.data[1] = 0x00;
+    message.data[2] = 0x00;
+    message.data[3] = 0x00;
+    message.data[4] = values[0];
+    message.data[5] = values[1];
+    message.data[6] = values[2];
+    message.data[7] = 0x01;
+    mcp2515_bit_modify(CANCTRL, (1<<REQOP2)|(1<<REQOP1)|(1<<REQOP0), 0);
+    mcp2515_send_message(&message);
+
+  }
+
+  if (digitalRead(CLICK) == 0) {
+    Serial.println("Click");
+  }      
+  
+  delay(250);
+}
+
+unsigned long hash(long data)
+{
+  return (groupId + generatedKey + counter + data) * 5325 % 16777216;
+}
+
+unsigned long generateKey(long previousKey, long randomValue)
+{
+  return (previousKey + randomValue + keyCounter) % 100000000;
+}
+
+void renewKey(){
+  tCAN message;
+  keyCounter++;
+  randomValue = rand() % 65536;
+  values[0] = randomValue;
+  values[1] = randomValue >> 8;
+
+  Serial.print("Random Value: ");
+  Serial.println(keyRenewHashRes, HEX);
+
+  unsigned long keyRenewHashRes = keyRenewHash(generatedKey, randomValue, keyCounter);
+  values[2] = keyRenewHashRes;
+  values[3] = keyRenewHashRes >> 8;
+  values[4] = keyRenewHashRes >> 16;
+
+  Serial.print("Computed Key Hash: ");
+  Serial.println(keyRenewHashRes, HEX);
+
+  message.id = 0x02;
+  message.header.rtr = 0;
+  message.header.length = 8;
+  message.data[0] = 0x00;
+  message.data[1] = 0x00;
+  message.data[2] = values[2];
+  message.data[3] = values[3];
+  message.data[4] = values[4];
+  message.data[5] = values[0];
+  message.data[6] = values[1];
+  message.data[7] = keyCounter;
+  mcp2515_bit_modify(CANCTRL, (1<<REQOP2)|(1<<REQOP1)|(1<<REQOP0), 0);
+  mcp2515_send_message(&message);
+}
+
+unsigned long keyRenewHash(long genKey, long rValue, int c)
+{
+  return (genKey + rValue + c) * 5325 % 16777216;
+}
