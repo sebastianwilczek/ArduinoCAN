127b7e9032094366cde909506d62c79cb0e178bf
arnold <arnold8520@gmail.com>
2020-01-23 14:09:24 +0100

    Basic RW with hack and LED

--- basicReadWrite/RED_WRITE/RED_WRITE.ino
+++ basicReadWrite/RED_WRITE/RED_WRITE.ino
@@ -1,28 +1,8 @@
-/****************************************************************************** 
-Toni Klopfenstein @ SparkFun Electronics
-September 2015
-https://github.com/sparkfun/CAN-Bus_Shield
-
-This example sketch works with the CAN-Bus shield from SparkFun Electronics.
-
-It reads the joystick for user inputs. 
-
-Based off of joystick_demo.pde by Sukkin Pang.
-Original file available at http://skpang.co.uk/catalog/arduino-canbus-shield-with-usd-card-holder-p-706.html
-
-This code is beerware; if you see me (or any other SparkFun employee) at the local, 
-and you've found our code helpful, please buy us a round!
-
-For the official license, please check out the license file included with the library.
-
-Distributed as-is; no warranty is given.
-********************************************************************************/
 #include <Canbus.h>
 #include <defaults.h>
 #include <global.h>
 #include <mcp2515.h>
 #include <mcp2515_defs.h>
-#include <SHA3.h>
 
 
 /* Define Joystick connection pins */
@@ -49,39 +29,45 @@ void setup() {
   digitalWrite(CLICK, HIGH);
  
  //Initialize serial terminal connection
-  Serial.begin(9600);
-  Serial.println("Joystick Demo");
+  Serial.begin(115200);
 
  if(Canbus.init(CANSPEED_500))  //Initialise MCP2515 CAN controller at the specified speed
     Serial.println("CAN Init ok");
   else
     Serial.println("Can't init CAN");
     
-  delay(1000);
+  delay(500);
 
 }
 
 
 
 void loop() {
-  unsigned long psk = 214789542710544005;
-  unsigned char hash;
   
      tCAN message;
   //Scan analog pins. If pin reads low, print the corresponding joystick movement.
+   
+   //Release brake
    if (digitalRead(UP) == 0) {
      Serial.println("Up");
         message.id = 0x01;
         message.header.rtr = 0;
         message.header.length = 8;
-        hash = SHA3_256(psk + 0x01) & 0xff;
-        message.data =  hash;
+        message.data[0] = 0x00;
+        message.data[1] = 0x00;
+        message.data[2] = 0x00;
+        message.data[3] = 0x00;
+        message.data[4] = 0x00;
+        message.data[5] = 0x00;
+        message.data[6] = 0x00;
+        message.data[7] = 0x01;
         mcp2515_bit_modify(CANCTRL, (1<<REQOP2)|(1<<REQOP1)|(1<<REQOP0), 0);
         mcp2515_send_message(&message);
        
         
      }
       
+  //Brake release
    if (digitalRead(DOWN) == 0) {
       Serial.println("Down");
         message.id = 0x01;
@@ -99,11 +85,27 @@ void loop() {
         mcp2515_send_message(&message);
         }
       
+   //Nothing   
    if (digitalRead(LEFT) == 0) {
        Serial.println("Left");}
    
+   //Hack
    if (digitalRead(RIGHT) == 0) {
-       Serial.println("Right");}
+       Serial.println("Right");
+        message.id = 0x01;
+        message.header.rtr = 0;
+        message.header.length = 8;
+        message.data[0] = 0x00;
+        message.data[1] = 0x00;
+        message.data[2] = 0x00;
+        message.data[3] = 0x00;
+        message.data[4] = 0x00;
+        message.data[5] = 0x00;
+        message.data[6] = 0x00;
+        message.data[7] = 0x01;
+        mcp2515_bit_modify(CANCTRL, (1<<REQOP2)|(1<<REQOP1)|(1<<REQOP0), 0);
+        mcp2515_send_message(&message);
+       }
 
    if (digitalRead(CLICK) == 0) {
        Serial.println("Click");}      
